b5440f07e30c353b6eaf39002ca55260ec1182d5
pepe67 <pi@raspberrypi.(none)>
2015-01-25 10:27:47 +0100

    Clean project - Piotr

---
+++ src/ProjectBundle/Entity/User.php
@@ -0,0 +1,27 @@
+<?php
+// src/ProjectBundle/Entity/User.php
+
+namespace ProjectBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="Users")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
